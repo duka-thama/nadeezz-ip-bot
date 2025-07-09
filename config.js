fb40d8e014e639c0c7b020a758a97e3e467a027f
duka-thama <dukkapro@tutamail.com>
2025-07-09 21:43:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+module.exports = {
+    TELEGRAM_BOT_TOKEN: '7740835813:AAEcxZkfVyaT1nIEYSoGdLmG94dlvL2pEjk', // Replace with your actual bot token
+    CHAT_ID: '7903968734', // Replace with your actual chat ID
+};
