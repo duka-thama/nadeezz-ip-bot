4e5c0bc278bd2d60aa3dbd279566717fe180c56c
duka-thama <dukkapro@tutamail.com>
2025-07-09 22:04:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 
 module.exports = {
-    TELEGRAM_BOT_TOKEN: '7740835813:AAEcxZkfVyaT1nIEYSoGdLmG94dlvL2pEjk', // Replace with your actual bot token
-    CHAT_ID: '7903968734', // Replace with your actual chat ID
+    TELEGRAM_BOT_TOKEN: '8159243173:AAGv4jkxPT8anq9GhNjCvxvTnAM7ewunu7U', // Replace with your actual bot token
+    CHAT_ID: '5737807853', // Replace with your actual chat ID
 };
